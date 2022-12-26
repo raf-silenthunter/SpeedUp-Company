bf9872ccb905c65e0e3cb091d7fc2d7a3dd18df4
Rafał Matyja <raf.matyja@gmail.com>
2022-12-26 23:15:47 +0100

    add login modal final version

--- main.js
+++ main.js
@@ -244,3 +244,24 @@ const shiftSlider = (e) => {
 brandsBtns.forEach((btn)=> {
     btn.addEventListener("click", shiftSlider);
 });
+
+
+// OPEN/CLOSE LOGIN MODAL FNs 
+
+const modalBtns = [...document.querySelectorAll(".main-nav__sign-btn, .btn-close")];
+const loginModal = document.querySelector(".login-modal");
+
+const checkModalBtn = (e) => {
+    if (e.target.matches('.main-nav__sign-icon, .main-nav__sign-btn, .main-nav__sign-btn--lash, .main-nav__call-to-action')) return "open";
+    else if (e.target.matches(".btn-close__icon, .login-modal__btn-close, .btn-close, .btn-close__icon--is-rotated")) return "close";
+    else console.error("wrong element clicked or does not contain proper class!");
+}
+
+const changeModalState = (e) => {
+    let clickedBtn = checkModalBtn(e);
+    loginModal.classList.toggle("visible", clickedBtn === "open");
+}
+
+modalBtns.forEach((btn)=> {
+    btn.addEventListener("click", changeModalState);
+});
